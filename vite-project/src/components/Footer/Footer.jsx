a98745da46aae81b052d20e2ff593241384df13a
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-08-29 19:32:47 -0300

    criei os componentes

---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <h3>
+            Fim
+        </h3>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ vite-project/src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const Main = () => {
+
+  return (
+    <main>
+        <div>
+            <Header/>
+        </div>
+        
+
+    </main>
+  )
+}
+
+export default Main
